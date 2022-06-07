5dbf026e85a5523fe350e747bf9eb0b9eee6a4cc
Marcos Paulo Rodrigues <marcos.paulo.rr@outlook.com>
2022-06-07 18:04:08 -0300

    inserindo dados do banco na aplicação

--- bd.sql
+++ bd.sql
@@ -5,3 +5,10 @@
     sinopse TEXT,
     nota DECIMAL (2,1)
     )";
+
+"INSERT INTO filmes (id, titulo, poster, sinopse, nota) VALUES (
+    0,
+    'Vingadores',
+    'https://www.themoviedb.org/t/p/original/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg',
+    'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.',
+    9.9)";
